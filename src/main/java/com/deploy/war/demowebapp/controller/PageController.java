3179ad1c2b371c16624ad5cb19c7b13c27990f06
dhannya09 <dhannyaunair@gmail.com>
2024-04-30 20:07:37 +0530

    initial commit Class3-Assignment2

---
+++ src/main/java/com/deploy/war/demowebapp/controller/PageController.java
@@ -0,0 +1,13 @@
+package com.deploy.war.demowebapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/")
+    public String homePage(){
+        return "home";
+    }
+}
